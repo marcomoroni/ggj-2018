9e83732502df8493fc5b7b3ae8e10ac051eee7c9
Marco Moroni <marcomoroni394@gmail.com>
2018-01-27 14:01:09 +0000

    Default play button selection

--- GGJ 2018/Assets/Scripts/NavigationController.cs
+++ GGJ 2018/Assets/Scripts/NavigationController.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NavigationController : MonoBehaviour {
 
+    public Button startButton;
+    public Button quitButton;
+
 	// Use this for initialization
 	void Start () {
-		
+        startButton.Select();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+        // move up/down...
+
 	}
 }

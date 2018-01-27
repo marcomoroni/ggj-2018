f5be8c6ec6538aec4b8ddb7ea8c49b184fee2016
Marco Moroni <marcomoroni394@gmail.com>
2018-01-27 12:45:48 +0000

    Show high score in main menu

---
+++ GGJ 2018/Assets/Scripts/ShowHighScore.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowHighScore : MonoBehaviour {
+
+    private Text highScoreText;
+
+	// Use this for initialization
+	void Start () {
+        highScoreText = GetComponent<Text>();
+        highScoreText.text = PlayerPrefs.GetInt("High Score").ToString();
+    }
+
+}
